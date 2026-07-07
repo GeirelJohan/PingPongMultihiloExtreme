d563fc70d4a231740eb21d427e9cbee0084f750a
Geirel <Geirel@DESKTOP-5H54R8H>
2026-07-07 17:35:29 -0600

    Cree la estructura de la interfaz con paneles

---
+++ src/vista/BottomPanel.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vista;
+
+/**
+ *
+ * @author Geirel
+ */
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+
+public class BottomPanel extends JPanel{
+    
+    public BottomPanel() {
+
+        setBackground(Color.GRAY);
+        setPreferredSize(new Dimension(900, 60));
+
+    }
+}
