dfd67151c6bf8c288e836aeb88b46da9271c7624
Geirel <Geirel@DESKTOP-5H54R8H>
2026-07-07 17:51:42 -0600

    Ahora agregue la parte de arriba con el nombre, puntaje y el temporizador

--- src/vista/TopPanel.java
+++ src/vista/TopPanel.java
@@ -10,14 +10,51 @@ package vista;
  */
 import java.awt.Color;
 import java.awt.Dimension;
-import javax.swing.JPanel;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class TopPanel extends JPanel{
+    
+    private JLabel lblJugador1;
+    private JLabel lblPuntos1;
+    private JLabel lblTiempo;
+    private JLabel lblJugador2;
+    private JLabel lblPuntos2;
     
     public TopPanel() {
 
         setBackground(Color.DARK_GRAY);
         setPreferredSize(new Dimension(900, 70));
+        setLayout(new FlowLayout(FlowLayout.CENTER, 25, 20));
+
+        Font fuente = new Font("Arial", Font.BOLD, 16);
+
+        lblJugador1 = new JLabel("Jugador 1");
+        lblJugador1.setForeground(Color.WHITE);
+        lblJugador1.setFont(fuente);
+
+        lblPuntos1 = new JLabel("Puntos: 0");
+        lblPuntos1.setForeground(Color.WHITE);
+        lblPuntos1.setFont(fuente);
+
+        lblTiempo = new JLabel("Tiempo: 60");
+        lblTiempo.setForeground(Color.YELLOW);
+        lblTiempo.setFont(fuente);
+
+        lblJugador2 = new JLabel("Jugador 2");
+        lblJugador2.setForeground(Color.WHITE);
+        lblJugador2.setFont(fuente);
+
+        lblPuntos2 = new JLabel("Puntos: 0");
+        lblPuntos2.setForeground(Color.WHITE);
+        lblPuntos2.setFont(fuente);
+
+        add(lblJugador1);
+        add(lblPuntos1);
+        add(lblTiempo);
+        add(lblJugador2);
+        add(lblPuntos2);
     }
 }
